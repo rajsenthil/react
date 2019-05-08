6cacc54abda6eb1006a8df6f3149d02a8f232617
Senthil Rajendran <senthilar@gmail.com>
2019-05-08 08:35:00 -0700

    Async calls

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
+  state = {images: []};
 
-  onSearchSubmit(term) {
-    console.log("Apps"+ term);
+  onSearchSubmit = async (term) => {
+    // console.log("Apps"+ term);
+    const response = await 
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers: {
+        Authorization: 'Client-ID 7f558adb729902a609fb6c7e1447a62a9ad9ade8b7c9e58b73a3c52769f33149'
+      }
+    });
+    
+    this.setState({images: response.data.results})
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSearchSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     );
   }
