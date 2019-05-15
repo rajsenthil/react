1d8d0ec23ce3b40143e8c640706ec0f4a86fd408
Senthil Rajendran <senthilar@gmail.com>
2019-05-15 08:21:25 -0700

    fixed

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,7 +2,7 @@ import jsonplaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonplaceholder.get('/posts');
-    dispatch({type: 'FETCH_POSTS', payload: response});
+    dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
 
 export const selectPost = (post) => {
