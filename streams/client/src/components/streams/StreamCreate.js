1b346a3f763816f5c1bae1be598bd1ed26ababf8
Senthil Rajenderan <senthilar@gmail.com>
2019-05-20 15:45:53 -0700

    Added input

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;    
+class StreamCreate extends React.Component {
+    renderInput({input}) {
+        return (            
+            <input {...input} />
+        )
+    }
+
+    handleTitleChange() {
+        console.log("Title changed");
+    }
+
+
+    handleDescChange() {
+        console.log("Description changed");
+    }
+
+    render() {
+        return (
+            <form>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
+            </form>
+        );
+    }
 }
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
